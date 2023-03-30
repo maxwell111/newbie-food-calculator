b81f62aeb628ad9619850a4fcc5b8652cf8e16e5
alexander.kartashev <alexander.kartashev@mvfglobal.com>
2023-03-30 10:18:01 +0300

    fixed bug with apiurl

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,7 @@
-const APIurl = "http://localhost:3000/data/";
+const APIurl =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000/data/"
+    : "https://gist.githubusercontent.com/maxwell111/03ddeb33c4435674bd4b0a917740e7b8/raw/f4b6bbc11632fdca12f0ddc07131c9323f0e3993/foodItems.json";
 
 export async function getSingleFoodItem(id) {
   const response = await fetch(`${APIurl}?id=${id}`);
