68f19b67fc8bb55f5852de609e7ebe9e52ffc8f7
alexander.kartashev <alexander.kartashev@mvfglobal.com>
2023-03-30 10:41:57 +0300

    update url for api calls

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 const APIurl =
   import.meta.env.MODE === "development"
     ? "http://localhost:3000/data/"
-    : "https://gist.githubusercontent.com/maxwell111/03ddeb33c4435674bd4b0a917740e7b8/raw/f4b6bbc11632fdca12f0ddc07131c9323f0e3993/foodItems.json";
+    : "https://my-json-server.typicode.com/maxwell111/newbie-food-calculator/data";
 
 export async function getSingleFoodItem(id) {
   const response = await fetch(`${APIurl}?id=${id}`);
